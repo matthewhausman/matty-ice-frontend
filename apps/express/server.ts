4a2e5cfb76304c590e05e04cfd12fda12f912707
Matthew Hausman <matthewhausman7@gmail.com>
2023-12-02 16:28:08 -0800

    upd

---
+++ apps/express/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+// Constants
+const port = process.env.PORT || 5173
+
+// Create http server
+const app = express()
+
+app.use('*', async (req, res) => {})
+
+// Start http server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`)
+})
