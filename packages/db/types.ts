4b48d97e57590fb049a9327edad66ca5f638657d
Matthew Hausman <matthewhausman7@gmail.com>
2023-12-03 22:32:23 -0800

    Update types.ts

--- packages/db/types.ts
+++ packages/db/types.ts
@@ -47,31 +47,12 @@ type BinaryFilters<
   [Column in keyof Columns as `${string & Column}_desc`]?: boolean
 }
 
-// type AndOrFilters<T extends MyTable> = {
-//   [Key in keyof BinaryFilters<T>]?:
-//     | BinaryFilters<T>[Key]
-//     | {
-//         and?: BinaryFilters<T>[Key][]
-//         or?: BinaryFilters<T>[Key][]
-//       }
-// }
-
-// type NotFilter<T extends MyTable, PrevFilter = AndOrFilters<T>> = {
-//   [Key in keyof PrevFilter]?:
-//     | PrevFilter[Key]
-//     | {
-//         not?: PrevFilter[Key]
-//       }
-// }
-
-type BinaryFiltersWithAnd = {}
-
 type AndOrMetaFilters<
   T extends MyTable,
   PrevFilter = BinaryFilters<T>,
 > = PrevFilter & {
-  and?: Omit<AndOrMetaFilters<T>, 'and'>[]
-  or?: Omit<AndOrMetaFilters<T>, 'or'>[]
+  and?: Prettify<Omit<AndOrMetaFilters<T>, 'and'>>[]
+  or?: Prettify<Omit<AndOrMetaFilters<T>, 'or'>>[]
 }
 
 type Schema = typeof schema
@@ -79,13 +60,15 @@ type Schema = typeof schema
 export type GenerateSearcher<
   T extends MyTable,
   R = ExtractTableRelationsFromSchema<Schema, T['_']['name']>,
-> = AndOrMetaFilters<T> & {
-  [Key in keyof Schema as Key extends keyof R
-    ? `with_${string & Key}`
-    : never]?: Schema[Key] extends MyTable
-    ? GenerateSearcher<Schema[Key]> | boolean
-    : never
-} & {
-  limit?: number
-  offset?: number
-}
+> = Prettify<
+  AndOrMetaFilters<T> & {
+    [Key in keyof Schema as Key extends keyof R
+      ? `with_${string & Key}`
+      : never]?: Schema[Key] extends MyTable
+      ? GenerateSearcher<Schema[Key]> | boolean
+      : never
+  } & {
+    limit?: number
+    offset?: number
+  }
+>
