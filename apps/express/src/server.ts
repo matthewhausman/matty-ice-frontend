a8f0b9234fa8dca6607c8f835c40c8d22eaff17f
Matthew Hausman <matthewhausman7@gmail.com>
2023-12-04 00:23:18 -0800

    upd

--- apps/express/src/server.ts
+++ apps/express/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { getMany } from './routes/users/getMany'
 // Constants
+import cors from 'cors'
 const port = process.env.PORT || 5174
 
 // Create http server
@@ -10,5 +11,14 @@ const app = express()
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`)
 })
+
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }),
+)
 
 app.get('/users', getMany)
