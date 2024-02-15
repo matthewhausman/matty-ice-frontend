a8ed8c654888891a3b0b92d8a77317737f24c1d4
Matthew Hausman <matthewhausman7@gmail.com>
2024-02-15 01:07:12 -0800

    Update Child.tsx

--- apps/react/src/components/Child.tsx
+++ apps/react/src/components/Child.tsx
@@ -5,7 +5,6 @@ import useUsers from '../queries/useUsers'
 export default function Child() {
   const usersQuery2 = useUsers({
     limit: 20,
-
     not: {
       or: [
         { id_gt: 1000, id_lt: 10 },
@@ -13,6 +12,7 @@ export default function Child() {
       ],
       and: [{ id_gt: 1000, id_lt: 10 }],
     },
+
     with_posts: {
       author_id_lte: 1234444,
     },
